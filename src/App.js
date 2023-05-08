0b8120f4a2195ee016da647e244bd249f07913ac
KDG <airedale63@naver.com>
2023-05-08 21:41:12 +0900

    할 일 리스트 만들기

--- src/App.js
+++ src/App.js
@@ -1,38 +1,120 @@
-import React, { useState, useEffect } from "react";
+import {React, useState, useRef, useEffect} from "react";
+import { AppBar, TextField, Toolbar, Button } from "@mui/material";
 
-let AppCallCount = 0;
+function useTodosState() {
+  const [todos, setTodos] = useState([]);
+  const lastTodoIdRef = useRef(0);
+
+  const addTodo = (newContent) => {
+    const id = ++lastTodoIdRef.current;
+
+    const newTodo = {
+      id,
+      content: newContent,
+      regDate: dateToStr(new Date())
+    };
+
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+  };
+
+  const modifyTodo = (index, newContent) => {
+    const newTodos = todos.map((todo, _index) => _index != index ? todo : { ...todo, content: newContent });
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, _index) => _index != index);
+    setTodos(newTodos);
+  };
+
+  return {
+    todos,
+    addTodo,
+    modifyTodo,
+    removeTodo
+  };
+}
 
 function App() {
-  AppCallCount++;
-  console.log(`AppCallCount :  ${AppCallCount}`);
-  const [no, setNo] = useState(0);
-  const [isdark, setIsdark] = useState(false);
+  const todosState = useTodosState();
 
   useEffect(()=> {
-    const html = document.getElementsByTagName('html')[0];
-    if(isdark){
-      html.classList.add("dark");
-    }else{
-      html.classList.remove("dark");
+    todosState.addTodo("운동");
+    todosState.addTodo("독서");
+    todosState.addTodo("공부");
+  }, [])
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    
+    const form = e.target;
+    
+    form.content.value = form.content.value.trim();
+    
+    if ( form.content.value.length == 0 ) {
+      alert('할일을 입력해주세요.');
+      form.content.focus();
+      
+      return;
     }
-  })
+    
+    todosState.addTodo(form.content.value);
+    form.content.value = '';
+    form.content.focus();
+  }
 
   return (
     <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <div className="flex-1"></div>
+          <span>My Note</span>
+          <div className="flex-1"></div>
+        </Toolbar>  
+      </AppBar>
+
+      <form onSubmit={onSubmit} className="flex flex-col mt-5 px-5 gap-2">
+      <TextField autoComplete="off" name="content" type="text" placeholder="할일을 입력해주세요." label="할 일을 입력해 주세요." variant="outlined"/>
+      <Button variant="contained" >추가</Button>
+      <Button variant="contained" >취소</Button>
+      </form>
       <div>
-        <button className="btn btn-outline" onClick={() => setNo(no + 1)}>
-          APP 버튼 : {no}
-        </button>
-        <hr />
-        <button className="btn btn-outline" onClick={() => setIsdark(!isdark)}>
-          테마토글
-        </button>
-        <hr />
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur dolorem est a optio velit deserunt qui, excepturi ullam voluptas. Voluptatibus vel nisi illum molestiae doloremque, sequi libero eum ducimus minima?</div>
-        <h1 className="color-primary">안녕 반가워</h1>
+        <ul>
+          {todosState.todos.map((todo) => (
+            <li key = {todo.id} className="mt-10">
+              <div className="flex gap-2">
+                <span>번호 : {todo.id}</span>
+                <span>날짜 : {todo.regDate}</span>
+              </div>
+              <div>{todo.content}</div>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
 }
 
 export default App;
+
+//날짜 객체를 입력받아 문장 형태로 반환
+function dateToStr(d) {
+  const pad = (n) => {
+    return n < 10 ? "0" + n : n;
+  }
+
+  return (
+    d.getFullYear() +
+    "-" +
+    pad(d.getMonth() + 1) +
+    "-" +
+    pad(d.getDate()) +
+    " " +
+    pad(d.getHours()) +
+    ":" +
+    pad(d.getMinutes()) +
+    ":" +
+    pad(d.getSeconds())
+  );
+}
